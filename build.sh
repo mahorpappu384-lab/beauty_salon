788c008949125bb3bbf32089b5e8cf37a6d33a1d
geetagalaxyadvertising2002 <geetagalaxyadvertising2002@gmail.com>
2026-05-09 06:06:07 -0700

    added build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+pip install --upgrade pip
+
+pip install setuptools
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+
+python manage.py migrate
